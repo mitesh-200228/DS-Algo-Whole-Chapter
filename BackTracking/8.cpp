cd0f2b39e6a7e4a23ad70b1d4826cfdc0f12da9f
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2022-01-31 14:25:59 +0530

    gs

--- BackTracking/8.cpp
+++ BackTracking/8.cpp
@@ -1,35 +1,40 @@
-#include<unordered_set>
-#include<stack>
-#include<iostream>
-#include<vector>
-#include<string>
-#include<numeric>
-#include<algorithm>
-#include<queue>
-#include<stdio.h>
-#include<cstring>
-#include<stack>
-#include<map>
-#include<unordered_map>
-#include<set>
-#include<climits>
-#include<cmath>
-#include<math.h>
-#include<limits>
+#include <unordered_set>
+#include <stack>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <cstring>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <climits>
+#include <cmath>
+#include <math.h>
+#include <limits>
 #define ll long long int
-const ll N = 1e5+ 2,MOD = 1e9+7;
+const ll N = 1e5 + 2, MOD = 1e9 + 7;
 using namespace std;
-int power(ll a,ll b) {
+int power(ll a, ll b)
+{
     ll ans = 1;
-    while(b>0){
-        if(b%2){
-            ans=ans*a;
+    while (b > 0)
+    {
+        if (b % 2)
+        {
+            ans = ans * a;
         }
-        a=a*a;b>>=1;
+        a = a * a;
+        b >>= 1;
     }
     return ans;
 }
-ll countSetBits(ll n) {
+ll countSetBits(ll n)
+{
     ll count = 0;
     while (n)
     {
@@ -38,52 +43,67 @@ ll countSetBits(ll n) {
     }
     return count;
 }
- 
-ll gcd(ll a, string b){
+
+ll gcd(ll a, string b)
+{
     ll res = 0;
-    for (int i = 0; i < b.length(); i++){
+    for (int i = 0; i < b.length(); i++)
+    {
         res = ((res * 10) + (b[i] - '0')) % a;
     }
-    return __gcd(a,res);
+    return __gcd(a, res);
 }
 
-bool PArtitioArray(vector<ll> &v,ll n,ll dp[][],ll sum){
-    if(n == 1){
-        return false;
-    }
-    if(dp[n][sum]!=-1){
-        return dp[n][sum];
-    }
-    return PArtitioArray(v,n-1,dp,sum-v[i])
-}
-
-void solve(){
-    ll n;cin>>n;
-    vector<ll> v(n);
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
     ll sum = 0;
-    for(ll i=0;i<n;i++){
-        cin>>v[i];
-        sum += v[i];
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
     }
-    bool dp[n+1][sum+1];
-    memset(dp,-1,sizeof(dp));
-    if(sum%2 != 0){
+    if(sum%2) {
         cout<<"0"<<endl;
-    }else{
-        ll i = 0;
-        cout<<PArtitioArray(v,n,dp,sum)<<endl;
+        return;
     }
+    sum/=2;
+    vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < 1; j++)
+        {
+            dp[i][j] = true;
+        }
+    }
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < sum + 1; j++)
+        {
+            if (arr[i - 1] <= j)
+            {
+                dp[i][j] = (dp[i - 1][j - arr[i - 1]] or dp[i - 1][j]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    cout<<dp[n][sum]<<endl;
 }
- 
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
- 
+
     ll t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         solve();
     }
--- c.cpp
+++ c.cpp
@@ -1,33 +1,40 @@
-#include<unordered_set>
-#include<stack>
-#include<iostream>
-#include<vector>
-#include<string>
-#include<numeric>
-#include<algorithm>
-#include<queue>
-#include<stack>
-#include<map>
-#include<unordered_map>
-#include<set>
-#include<climits>
-#include<cmath>
-#include<math.h>
-#include<limits>
+#include <unordered_set>
+#include <stack>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <numeric>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <climits>
+#include <cmath>
+#include <math.h>
+#include <limits>
 #define ll long long int
-const ll N = 1e5+ 2,MOD = 1e9+7;
+const ll N = 1e5 + 2, MOD = 1e9 + 7;
 using namespace std;
-int power(ll a,ll b) {
+int power(ll a, ll b)
+{
     ll ans = 1;
-    while(b>0){
-        if(b%2){
-            ans=ans*a;
+    while (b > 0)
+    {
+        if (b % 2)
+        {
+            ans = ans * a;
         }
-        a=a*a;b>>=1;
+        a = a * a;
+        b >>= 1;
     }
     return ans;
 }
-ll countSetBits(ll n) {
+ll countSetBits(ll n)
+{
     ll count = 0;
     while (n)
     {
@@ -36,42 +43,53 @@ ll countSetBits(ll n) {
     }
     return count;
 }
- 
-ll gcd(ll a, string b){
+
+ll gcd(ll a, string b)
+{
     ll res = 0;
-    for (int i = 0; i < b.length(); i++){
+    for (int i = 0; i < b.length(); i++)
+    {
         res = ((res * 10) + (b[i] - '0')) % a;
     }
-    return __gcd(a,res);
+    return __gcd(a, res);
 }
- 
-void solve(){
-    ll n;cin>>n;
-    ll k;cin>>k;
-    string s;cin>>s;
-    ll x = 0;
-    ll ans = 0;
-    for(ll i=0;i<n-k+1;i++){
-        x = stoi(s.substr(i,k));
-        ans = ans^x;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll k;
+    cin >> k;
+    string s;
+    cin >> s;
+    ll x = INT_MIN;
+    ll l = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        for (ll j = i; j < n; j++)
+        {
+            if (s[j] < s[j + 1])
+            {
+                x = max(x, to_string(s[j]));
+                l++;
+            }else{
+                break;
+            }
+        }
+        ans += l;
+        i = j + 1;
     }
-    string g = to_string(ans);
-    ll op = 0;
-    for(ll i=0;i<g.length();i++){
-        if(g[i] == '1') op++;
-    }
-    cout<<op<<endl;
 }
- 
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
- 
+
     ll t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         solve();
     }
