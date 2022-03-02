f1827d3ffbc0b17f2905ccf3061bc411acbe27d1
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2022-03-02 18:15:11 +0530

    new question

--- Codechef/1700-1800/CHEFRES.cpp
+++ Codechef/1700-1800/CHEFRES.cpp
@@ -1,97 +1,38 @@
-#include<unordered_set>
-#include<stack>
-#include<iostream>
-#include<vector>
-#include<string>
-#include<numeric>
-#include<cstring>
-#include<algorithm>
-#include<queue>
-#include <stdio.h>
-#include<stack>
-#include<map>
-#include<unordered_map>
-#include<set>
-#include<climits>
-#include<cmath>
-#include<math.h>
-#include<limits>
-#define ll long long int
-const ll N = 1e5+ 2,MOD = 1e9+7;
+#include <bits/stdc++.h>
 using namespace std;
-int power(ll a,ll b) {
-    ll ans = 1;
-    while(b>0){
-        if(b%2){
-            ans=ans*a;
-        }
-        a=a*a;b>>=1;
-    }
-    return ans;
-}
-ll countSetBits(ll n) {
-    ll count = 0;
-    while (n)
-    {
-        count += n & 1;
-        n >>= 1;
-    }
-    return count;
-}
- 
-ll gcd(ll a, string b){
-    ll res = 0;
-    for (int i = 0; i < b.length(); i++){
-        res = ((res * 10) + (b[i] - '0')) % a;
-    }
-    return __gcd(a,res);
-}
- 
-void solve(){
-    ll n;cin>>n;
-    ll m;cin>>m;
-    std::vector<std::pair<ll,ll>> p(n);
-    std::vector<ll> v(m);
-    std::vector<ll> ans(10e9,0);
-    ll j = n;
-    while(j) {
-        ll a,b;cin>>a;cin>>b;
-        for(ll i=a;i<=b;i++){
-            ans[i] = 1;
-        }
-        j--;
-    }
-    ll k = m;
-    while(k--) {
-        ll x;cin>>x;
-        if(ans[x] && ans[x+1]){
-            cout<<"0"<<endl;
-            continue;
-        }else if(ans[x] && !ans[x+1]){
-            ll p = x;
-            while(!ans[p]){
-                p++;
-            }
-            if(p<10e9) {
-                cout<<abs(p-x+1)<<endl;
-            }else{
-                cout<<"-1"<<endl;
-            }
-        }
-    }
-}
- 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
- 
-    ll t;
-    cin>>t;
-    while(t--)
-    {
-        solve();
-    }
-    return 0;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    vector<pair<int,int>> intervals;
+	    while(n--){
+	        int start,end;
+	        cin>>start>>end;
+	        intervals.push_back(make_pair(start,end));
+	    }
+	    sort(intervals.begin(),intervals.end());
+	    while(m--){
+	        int curr;
+	        cin>>curr;
+	        int position = lower_bound(intervals.begin(),intervals.end(),make_pair(curr,0)) - intervals.begin();
+	        if(position==0){
+	            int ans = intervals[0].first - curr;
+	            cout<<ans<<endl;
+	        }
+	        else{
+	            int ans=-1;
+	            if(intervals[position-1].second > curr){
+	                ans = 0;
+	            }
+	            else if(position<intervals.size()){
+	                ans = intervals[position].first - curr;
+	            }
+	            cout<<ans<<endl;
+	        }
+	    }
+	}
+	return 0;
 }
