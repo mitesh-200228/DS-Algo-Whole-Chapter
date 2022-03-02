8fb13a10a905fcb07f94bb850b837b5f21193c9c
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2022-03-03 00:50:20 +0530

    LatestOne

--- Codechef/1700-1800/AND.cpp
+++ Codechef/1700-1800/AND.cpp
@@ -1,69 +1,38 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define inf 1e18
-#define mod 1000000007
-#define gcd(a, b) __gcd(a, b)
-bool bit(ll a, ll j)
-{
-    ll newno = a >> (j - 1);
-    return newno & 1;
-}
+#define ll int long long
+
 void solve()
 {
-
-    ll n;
+    ll n, i, ans = 0, j;
     cin >> n;
     ll a[n];
-    for (ll i = 0; i < n; i++)
-    {
+    vector<ll> v[32];
+    for (i = 0; i < n; i++)
         cin >> a[i];
-    }
-    ll b[n][30];
-    for (ll i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        for (ll j = 0; j < 30; j++)
+        for (j = 0; j < 32; j++)
         {
-            if (bit(a[i], j + 1))
-            {
-                b[i][j] = 1;
-            }
-            else
-            {
-                b[i][j] = 0;
-            }
+            if (a[i] & 1)
+                v[j].push_back(1);
+            a[i] /= 2;
         }
     }
-    ll count = 0, sum = 0;
-    for (ll i = 0; i < 30; i++)
-    {
-        count = 0;
-        for (ll j = 0; j < n; j++)
-        {
-            // cout<<b[j][i];
-            if (b[j][i] == 1)
-                count++;
-        }
-        count = (count * (count - 1)) / 2;
-        // cout<<count;
-        sum = sum + ((1 << i) * count);
-        // cout<<endl;
+    for (i = 0; i < 32; i++){
+        ll p = v[i].size();
+        ans += (p * (p - 1) / 2) * pow(2, i);
     }
-    cout << sum;
-    cout << endl;
+    cout << ans << endl;
 }
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll t = 1;
-    // cin>>t;
-    while (t--)
-    {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1; 
+    while (t--){
         solve();
     }
-
-    return 0;
 }
