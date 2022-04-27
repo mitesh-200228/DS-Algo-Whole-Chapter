9814896b09a31f03265f32222863f7a2cba957a6
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2022-04-28 01:40:11 +0530

    latest one

--- Searching & Sorting/2.cpp
+++ Searching & Sorting/2.cpp
@@ -1,33 +1,73 @@
-// { Driver Code Starts
-#include <bits/stdc++.h>
+#include<unordered_set>
+#include<stack>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<climits>
+#include<cmath>
+#include<math.h>
+#include<limits>
+ 
+#define ll long long int
+const ll N = 1e5+ 2,MOD = 1e9+7;
 using namespace std;
-
- // } Driver Code Ends
-class Solution {
-  public:
-    int countSquares(int N) {
-        // code here
-        int x = 0;
-        if(sqrt(N) == floor(sqrt(N))){
-            x = sqrt(N)-1;   
-        }else{
-            x = floor(sqrt(N));
+ 
+// best for finding power of a^b in O(logn)
+int power(ll a,ll b) {
+    ll ans = 1;
+    while(b>0){
+        if(b%2){
+            ans=ans*a;
         }
-        return x;
+        a=a*a;b>>=1;
     }
-};
-
-// { Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        
-        cin>>N;
-
-        Solution ob;
-        cout << ob.countSquares(N) << endl;
+    return ans;
+}
+ 
+ll gcd(ll a, string b){
+    ll res = 0;
+    for (int i = 0; i < b.length(); i++){
+        res = ((res * 10) + (b[i] - '0')) % a;
+    }
+    return __gcd(a,res);
+}
+void solve(){
+    ll n;cin>>n;
+    vector<ll> v(n);
+    for(ll i=0;i<n;i++){
+        cin>>v[i];
+    }
+    bool flag = true;
+    for(ll i=0; i<n ;i++){
+        if(v[i] == i+1){
+            cout<<i<<endl;
+            flag = false;
+            break;
+        }   
+    }
+    if(flag){
+        cout<<"-1"<<endl;
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
     }
     return 0;
-}  // } Driver Code Ends
+}
--- Searching & Sorting/4.cpp
+++ Searching & Sorting/4.cpp
@@ -1,75 +1,33 @@
-#include<unordered_set>
-#include<stack>
-#include<iostream>
-#include<vector>
-#include<string>
-#include<numeric>
-#include<algorithm>
-#include<queue>
-#include<stack>
-#include<map>
-#include<unordered_map>
-#include<set>
-#include<climits>
-#include<cmath>
-#include<math.h>
-#include<limits>
- 
-#define ll long long int
-const ll N = 1e5+ 2,MOD = 1e9+7;
+// { Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
- 
-// best for finding power of a^b in O(logn)
-int power(ll a,ll b) {
-    ll ans = 1;
-    while(b>0){
-        if(b%2){
-            ans=ans*a;
-        }
-        a=a*a;b>>=1;
-    }
-    return ans;
-}
- 
-ll gcd(ll a, string b){
-    ll res = 0;
-    for (int i = 0; i < b.length(); i++){
-        res = ((res * 10) + (b[i] - '0')) % a;
-    }
-    return __gcd(a,res);
-}
-void solve(){
-    ll n;cin>>n;
 
-    ll l=0;ll h=n-1;
-    ll ans = 0;
-    while(l<=h){
-        ll mid = l + (h-l)/2;
-        if(mid*mid == n){
-            ans = mid;
-            break;
-        }else if(mid*mid < n){
-            l = mid+1;
-            ans = mid;
+ // } Driver Code Ends
+class Solution {
+  public:
+    int countSquares(int N) {
+        // code here
+        int x = 0;
+        if(sqrt(N) == floor(sqrt(N))){
+            x = sqrt(N)-1;   
         }else{
-            h = mid - 1;
+            x = floor(sqrt(N));
         }
-        cout<<mid<<" "<<ans<<" "<<endl;
+        return x;
     }
-    // cout<<ans<<endl;
-}
- 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
- 
-    ll t;
-    cin>>t;
-    while(t--)
-    {
-        solve();
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.countSquares(N) << endl;
     }
     return 0;
-}
+}  
