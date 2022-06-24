fd8c3326e5591b9b1ef32ff48b3deed851237274
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2022-06-25 02:24:39 +0530

    latest one

--- Dynamic Programming/LB25.cpp
+++ Dynamic Programming/LB25.cpp
@@ -1,77 +1,26 @@
-#include<unordered_set>
-#include<stack>
-#include<iostream>
-#include<vector>
-#include<string>
-#include<numeric>
-#include<cstring>
-#include<algorithm>
-#include<queue>
-#include <stdio.h>
-#include<stack>
-#include<map>
-#include<unordered_map>
-#include<set>
-#include<climits>
-#include<cmath>
-#include<math.h>
-#include<limits>
-#define ll long long int
-const ll N = 1e5+ 2,MOD = 1e9+7;
-using namespace std;
-int power(ll a,ll b) {
-    ll ans = 1;
-    while(b>0){
-        if(b%2){
-            ans=ans*a;
-        }
-        a=a*a;b>>=1;
-    }
-    return ans;
-}
-ll countSetBits(ll n) {
-    ll count = 0;
-    while (n)
-    {
-        count += n & 1;
-        n >>= 1;
-    }
-    return count;
-}
- 
-ll gcd(ll a, string b){
-    ll res = 0;
-    for (int i = 0; i < b.length(); i++){
-        res = ((res * 10) + (b[i] - '0')) % a;
-    }
-    return __gcd(a,res);
-}
- 
-void print(vector<ll> v,ll n){
-    for(ll i=0;i<n;i++){
-        cout<<v[i]<<' ';
-    }cout<<endl;
-}
- 
-void solve(){
-    ll n;cin>>n;
-    vector<ll> v(n);
-    for(ll i=0;i<n;i++){
-        cin>>v[i];
-    }
-}
- 
-int main()
+int dp[52][52];
+
+int area(int i, int j, vector<vector<int>> &mat)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
- 
-    ll t;
-    cin>>t;
-    while(t--)
+
+    if (i < 0 || j < 0)
+        return 0;
+
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    int left = area(i, j - 1, mat);
+    int top = area(i - 1, j, mat);
+    int diag = area(i - 1, j - 1, mat);
+
+    int side = 0;
+
+    if (mat[i][j] != 0)
     {
-        solve();
+        side = 1 + min({left, top, diag});
     }
-    return 0;
+
+    ans = max(ans, side);
+
+    return dp[i][j] = side;
 }
