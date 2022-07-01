237b6a050ee8ad70a3b9f5a38d7a50e72b7f5903
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2022-07-01 15:16:26 +0530

    latest one

--- Heap/12.cpp
+++ Heap/12.cpp
@@ -1,77 +1,60 @@
-#include<unordered_set>
-#include<stack>
-#include<iostream>
-#include<vector>
-#include<string>
-#include<numeric>
-#include<cstring>
-#include<algorithm>
-#include<queue>
-#include <stdio.h>
-#include<stack>
-#include<map>
-#include<unordered_map>
-#include<set>
-#include<climits>
-#include<cmath>
-#include<math.h>
-#include<limits>
-#define ll long long int
-const ll N = 1e5+ 2,MOD = 1e9+7;
+#include <bits/stdc++.h>
 using namespace std;
-int power(ll a,ll b) {
-    ll ans = 1;
-    while(b>0){
-        if(b%2){
-            ans=ans*a;
+
+class Solution
+{
+    priority_queue<int> s;
+    priority_queue<int, vector<int>, greater<int>> g;
+
+public:
+    void insertHeap(int &x){
+        if (s.size() == 0){
+            s.push(x);
+            return;
         }
-        a=a*a;b>>=1;
+        if (s.size() > g.size()){
+            if (x < s.top()){
+                g.push(s.top());
+                s.pop();
+                s.push(x);
+            }
+            else{
+                g.push(x);
+            }
+        }else{
+            if (x < s.top()){
+                s.push(x);
+            }
+            else{
+                g.push(x);
+                s.push(g.top());
+                g.pop();
+            }
+        }
     }
-    return ans;
-}
-ll countSetBits(ll n) {
-    ll count = 0;
-    while (n)
-    {
-        count += n & 1;
-        n >>= 1;
+
+    double getMedian(){
+        if (s.size() == g.size()){
+            return (double)(s.top() + g.top()) / 2.0;
+        }
+        else
+            return s.top();
     }
-    return count;
-}
- 
-ll gcd(ll a, string b){
-    ll res = 0;
-    for (int i = 0; i < b.length(); i++){
-        res = ((res * 10) + (b[i] - '0')) % a;
-    }
-    return __gcd(a,res);
-}
- 
-void print(vector<ll> v,ll n){
-    for(ll i=0;i<n;i++){
-        cout<<v[i]<<' ';
-    }cout<<endl;
-}
- 
-void solve(){
-    ll n;cin>>n;
-    vector<ll> v(n);
-    for(ll i=0;i<n;i++){
-        cin>>v[i];
-    }
-}
- 
+};
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
- 
-    ll t;
-    cin>>t;
-    while(t--)
-    {
-        solve();
+    int n, x;
+    int t;
+    cin >> t;
+    while (t--){
+        Solution ob;
+        cin >> n;
+        for (int i = 1; i <= n; ++i){
+            cin >> x;
+            ob.insertHeap(x);
+            cout << floor(ob.getMedian()) << endl;
+        }
     }
     return 0;
-}
+} 
