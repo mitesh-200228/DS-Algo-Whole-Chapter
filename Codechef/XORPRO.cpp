01ff971a9423e7e26544fcc82c1558e99ea89505
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2022-07-12 18:40:52 +0530

    latest one

--- Codechef/XORPRO.cpp
+++ Codechef/XORPRO.cpp
@@ -1,77 +1,44 @@
-#include<unordered_set>
-#include<stack>
-#include<iostream>
-#include<vector>
-#include<string>
-#include<numeric>
-#include<cstring>
-#include<algorithm>
-#include<queue>
-#include <stdio.h>
-#include<stack>
-#include<map>
-#include<unordered_map>
-#include<set>
-#include<climits>
-#include<cmath>
-#include<math.h>
-#include<limits>
-#define ll long long int
-const ll N = 1e5+ 2,MOD = 1e9+7;
+#include <bits/stdc++.h>
 using namespace std;
-int power(ll a,ll b) {
-    ll ans = 1;
-    while(b>0){
-        if(b%2){
-            ans=ans*a;
-        }
-        a=a*a;b>>=1;
-    }
-    return ans;
-}
-ll countSetBits(ll n) {
-    ll count = 0;
-    while (n)
-    {
-        count += n & 1;
-        n >>= 1;
-    }
-    return count;
-}
- 
-ll gcd(ll a, string b){
-    ll res = 0;
-    for (int i = 0; i < b.length(); i++){
-        res = ((res * 10) + (b[i] - '0')) % a;
-    }
-    return __gcd(a,res);
-}
- 
-void print(vector<ll> v,ll n){
-    for(ll i=0;i<n;i++){
-        cout<<v[i]<<' ';
-    }cout<<endl;
-}
- 
-void solve(){
-    ll n;cin>>n;
-    vector<ll> v(n);
-    for(ll i=0;i<n;i++){
-        cin>>v[i];
-    }
-}
- 
-int main()
+#define ar array
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define debug(x) cout << #x << " " << x << endl;
+#define print(v)     \
+    for (auto x : v) \
+        cout << x << " ";
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+const int M = 1e9 + 7;
+const int N = 1e5 + 5;
+
+void solve()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
- 
-    ll t;
-    cin>>t;
-    while(t--)
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int x = 0;
+    for (int i = 0; i < n; i++)
     {
-        solve();
+        cin >> a[i];
+        x ^= a[i];
     }
+    int ans = 1e16;
+    for (int i = 0; i < n; i++)
+    {
+        ans = min(ans, abs(a[i] - (x ^ a[i])));
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
